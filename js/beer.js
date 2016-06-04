7efeaf0e241041a34fc43af34ea71cdd513cd5aa
Jackie Gutierrez <seleneee@Jax.local>
2016-06-04 15:54:25 -0700

    Added brewerydb

--- js/beer.js
+++ js/beer.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.getJSON("http://api.brewerydb.com/v2/?key=ed05c35d525c392e64ef73a314c381ac&format=json",
+    $.getJSON("http://api.brewerydb.com/v2/?key=ed05c35d525c392e64ef73a314c381ac&format=json&callback=?",
         function(data){
           console.log(JSON.stringify(data));
     });
